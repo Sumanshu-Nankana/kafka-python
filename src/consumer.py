c0d7eafc90de0327befbb73cf167245d4cb3a0f5
Sumanshu Nankana <sumanshunankana@gmail.com>
2024-02-23 18:53:32 +0000

    added consumer code

---
+++ src/consumer.py
@@ -0,0 +1,51 @@
+import logging
+import os
+
+from confluent_kafka import Consumer
+
+import logging_config
+import utils
+
+
+class ConsumerClass:
+    def __init__(self, bootstrap_server, topic, group_id):
+        """Initializes the consumer."""
+        self.bootstrap_server = bootstrap_server
+        self.topic = topic
+        self.group_id = group_id
+        self.consumer = Consumer(
+            {"bootstrap.servers": bootstrap_server, "group.id": self.group_id}
+        )
+
+    def consume_messages(self):
+        """Consume Messages from Kafka"""
+        self.consumer.subscribe([self.topic])
+        logging.info(f"Successfully subscribed to topic: {self.topic}")
+
+        try:
+            while True:
+                msg = self.consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    logging.error(f"Consumer error: {msg.error()}")
+                    continue
+                logging.info(
+                    f"Received message: " f"{msg.value().decode('utf-8')}"
+                )  # noqa: E501
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.consumer.close()
+
+
+if __name__ == "__main__":
+    utils.load_env()
+    logging_config.configure_logging()
+
+    bootstrap_server = os.environ.get("KAFKA_BOOTSTRAP_SERVERS")
+    topic = os.environ.get("KAFKA_TOPIC")
+    group_id = os.environ.get("KAFKA_GROUP_ID", "my-consumer-group")
+
+    consumer = ConsumerClass(bootstrap_server, topic, group_id)
+    consumer.consume_messages()
